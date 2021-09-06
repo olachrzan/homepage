7882f54d897527e2af21b4c8059909b5ff2bff11
Ola Chrzan <ola.chrzan16@gmail.com>
2021-09-06 22:12:22 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,39 @@
-console.log("Hej! Cieszę się, że tu zajrzałeś :) Daj znać co myślisz o kodzie strony");
+{
+   console.log("Hej! Cieszę się, że tu zajrzałeś :) Daj znać co myślisz o kodzie strony");
 
-let body = document.querySelector(".js-body");
-let button = document.querySelector(".js-button");
-let mainContainer = document.querySelector(".js-mainContainer");
-let header = document.querySelectorAll(".js-header");
-let tableCellHeader = document.querySelectorAll(".js-table__cell--header");
-let navigation = document.querySelector(".js-navigation");
-let footer = document.querySelector(".js-footer");
+   const button = document.querySelector(".js-button");
+   const header = document.querySelectorAll(".js-header");
+   const tableCellHeader = document.querySelectorAll(".js-table__cell--header");
 
-button.addEventListener("click", () => {
-   body.classList.toggle("body--darkMode");
-   mainContainer.classList.toggle("mainContainer--darkMode");
-   navigation.classList.toggle("navigation--darkMode");
-   footer.classList.toggle("footer--darkMode");
-   button.classList.toggle("button--darkMode");
-   button.innerText = body.classList.contains("body--darkMode") ? "Jaśniejsze tło" : "Ciemniejsze tło";
-});
+   const setDarkMode = () => {
+      const body = document.querySelector(".js-body");
+      const mainContainer = document.querySelector(".js-mainContainer");
+      const navigation = document.querySelector(".js-navigation");
+      const footer = document.querySelector(".js-footer");
+      
+      body.classList.toggle("body--darkMode");
+      mainContainer.classList.toggle("mainContainer--darkMode");
+      navigation.classList.toggle("navigation--darkMode");
+      footer.classList.toggle("footer--darkMode");
+      button.classList.toggle("button--darkMode");
+      button.innerText = body.classList.contains("body--darkMode") ? "Jaśniejsze tło" : "Ciemniejsze tło";
+   };
 
-header.forEach(item => {
-   button.addEventListener('click', () => {
-      item.classList.toggle("header--darkMode")
-   })
-});
+   header.forEach(item => {
+      button.addEventListener('click', () => {
+         item.classList.toggle("header--darkMode")
+      })
+   });
 
-tableCellHeader.forEach(item => {
-   button.addEventListener('click', () => {
-      item.classList.toggle("table__cell--darkmode")
-   })
-});
+   tableCellHeader.forEach(item => {
+      button.addEventListener('click', () => {
+         item.classList.toggle("table__cell--darkmode")
+      })
+   });
+
+   const init = () => {
+      button.addEventListener("click", setDarkMode);
+   };
+
+   init();
+}
